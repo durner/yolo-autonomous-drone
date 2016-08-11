764a6107e54d43e6705d33ad1cfea9967977cf8b
Dominik Durner <durner@in.tum.de>
2016-08-11 11:42:38 +0200

    Added PID at swagetti YOLODrone

---
+++ PID.py
@@ -0,0 +1,17 @@
+class PID(object):
+    def __init__(self, K_p=0.3, K_d=0.05, K_i=0.00, dt=0.5):
+        self.K_p = K_p
+        self.K_d = K_d
+        self.K_i = K_i
+        self.dt = dt
+        self.w = 0
+        self.velocity = 0
+        self.errorsum = 0
+        self.actual_previous = 0
+
+    def step(self, desired, actual):
+        self.errorsum += (desired - actual) * self.dt
+        self.velocity = (actual - self.actual_previous) / self.dt
+        u = self.K_p * (desired - actual) + self.K_d * (self.w - self.velocity) + self.K_i * self.errorsum
+        self.actual_previous = actual
+        return u
---
+++ actuators.py
@@ -0,0 +1,36 @@
+from libardrone import libardrone
+
+from PID import PID
+
+class Actuator(object):
+    def __init__(self, drone, picture_width, desired_move):
+        self.turn = PID(K_p=0.3)
+        self.move = PID()
+        self.picture_width = picture_width
+        self.desired_move = desired_move
+        self.drone = drone
+
+    def step(self, wdithmid, width):
+        desired_turn = self.picture_width / 2
+        actual_turn = wdithmid
+        actual_move = width
+        ut = self.turn.step(desired_turn, actual_turn)
+        um = self.move.step(self.desired_move, actual_move)
+        self.drone.at(libardrone.at_pcmd, True, 0, self.moveDrone(um), 0, self.turnDrone(ut))
+
+    def turnDrone(self, u):
+        speed = - u / (self.picture_width / 2.)
+        print "move horizontal to" + str(speed)
+        return speed
+
+    def moveDrone(self, u):
+        speed = - u / self.picture_width
+        print "move near to" + str(speed)
+        return speed
+
+
+class VerticalActuator(object):
+    def __init__(self, drone):
+        self.drone = drone
+        self.drone.takeoff()
+        print "drone takeoff"
